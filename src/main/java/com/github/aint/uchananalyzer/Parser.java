d02b465038e7d100905ab561823f2e0292362d85
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-04-11 19:10:52 +0300

    Retrieve posts

--- src/main/java/com/github/aint/uchananalyzer/Parser.java
+++ src/main/java/com/github/aint/uchananalyzer/Parser.java
@@ -19,11 +19,25 @@ public class Parser {
         Document page = Jsoup.connect("http://uchan.to/lp").get();
         Set<String> topics = new HashSet<>(getThreadTopics(page));
         topics.forEach(System.out::println);
+
+        List<String> posts = getPosts(page);
+        posts.forEach(System.out::println);
+
     }
 
     private static List<String> getThreadTopics(Document page) {
         Elements topics = page.getElementsByAttributeValueContaining("title", "Тред:");
         return topics.stream().map(t -> t.attr("title")).collect(Collectors.toList());
+    }
+
+    private static List<String> getPosts(Document page) {
+        return page.getElementsByAttributeValueContaining("id", "post_text_").stream()
+                .map(p -> p.getElementsByTag("p"))
+                .collect(Collectors.toList())
+                .stream()
+                .map(Elements::text)
+                .filter(s -> !s.isEmpty())
+                .collect(Collectors.toList());
     }
 
 }
