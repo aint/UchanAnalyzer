2d0700e489dff96f186b271b5fa9bf4257e1baa3
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-04-17 19:31:23 +0300

    Save topic to JSON

--- src/main/java/com/github/aint/uchananalyzer/Parser.java
+++ src/main/java/com/github/aint/uchananalyzer/Parser.java
@@ -1,10 +1,16 @@
 package com.github.aint.uchananalyzer;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -26,10 +32,31 @@ public class Parser {
         Document page = Jsoup.connect(URL + LAST_PAGE).get();
         Set<String> topics = new HashSet<>(getThreadTopics(page));
         topics.forEach(System.out::println);
+//        save2Json(topics);
 
         List<String> posts = getPosts(page);
         posts.forEach(System.out::println);
 
+    }
+
+    private static void save2Json(Collection<String> collection) {
+        JsonArray array = new JsonArray();
+        for (String str : collection) {
+            JsonObject innerJson = new JsonObject();
+            innerJson.addProperty("topic", str);
+            array.add(innerJson);
+        }
+        JsonObject json = new JsonObject();
+        json.add(String.valueOf(LAST_PAGE), array);
+
+        try (Writer writer = new FileWriter(String.valueOf(LAST_PAGE) + ".json")) {
+            new GsonBuilder()
+                    .setPrettyPrinting()
+                    .create()
+                    .toJson(json, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static List<String> getThreadTopics(Document page) {
